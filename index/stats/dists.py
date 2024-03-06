91d02f20b3a21a80b0aa5ecc2ff53b5caa419195
StardustDL <stardustdl@163.com>
2024-03-06 15:01:48 +0800

    update dist stats

--- index/stats/dists.py
+++ index/stats/dists.py
@@ -7,3 +7,18 @@ from .shared import duration, success
 
 S.count(duration)
 S.count(success)
+
+
+@S.count
+def loc(data: Distribution):
+    return data.locCount
+
+
+@S.count
+def filesize(data: Distribution):
+    return data.fileSize
+
+
+@S.count
+def filecount(data: Distribution):
+    return data.fileCount
